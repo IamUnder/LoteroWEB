c382e815c7ad9c66cef380c0e741a1301dcb3195
Under <jorgeolmo.I@gmail.com>
2022-02-06 01:07:02 +0100

    Arreglo github pajes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 import { getWeek, getPrimitiva, getBonoloto, getEuromillon } from './function.js'
 
 window.addEventListener('load', () => {
-    getWeek(),
-    getPrimitiva(),
-    getBonoloto(),
-    getEuromillon()
+    getWeek();
+    getPrimitiva();
+    getBonoloto();
+    getEuromillon();
 })
